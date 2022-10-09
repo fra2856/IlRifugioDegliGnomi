5c92ce938da0ab0cb798676de69762c214a6b82f
MagicThunder02 <matteo.savina@gmail.com>
2022-10-09 17:42:11 +0200

    first section

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  pages = [
+    {
+      name: "Logo",
+      image: "/assets/icon/logo-white.svg"
+    },
+    {
+      name: "Home",
+      color: "light"
+    },
+    {
+      name: "Dove siamo",
+      color: "light"
+    },
+    {
+      name: "Servizi",
+      color: "light"
+    },
+    {
+      name: "Camere",
+      color: "light"
+    },
+    {
+      name: "Prenotazioni",
+      color: "light"
+    },
+  ]
+
+  constructor() { }
 
 }
