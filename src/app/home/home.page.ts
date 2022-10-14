841b734439e768c397380d82b2cf6745b383e191
MagicThunder02 <matteo.savina@gmail.com>
2022-10-14 14:27:15 +0200

    parallax non completo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { IonContent } from '@ionic/angular';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { DomController, IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,38 +9,60 @@ import { IonContent } from '@ionic/angular';
 export class HomePage {
 
   @ViewChild(IonContent) content: IonContent;
+  @ViewChild("parallax", { read: ElementRef }) image: ElementRef;
 
   pages = [
     {
       name: "Logo",
-      image: "/assets/icon/logo-white.svg"
+      image: "/assets/icon/logo-white.svg",
+      element: "section1"
     },
     {
       name: "Home",
-      color: "light"
+      color: "light",
+      element: "section1"
     },
     {
       name: "Dove siamo",
-      color: "light"
+      color: "light",
+      element: "section2"
     },
     {
       name: "Servizi",
-      color: "light"
+      color: "light",
+      element: "section3"
     },
     {
       name: "Camere",
-      color: "light"
+      color: "light",
+      element: "camere"
     },
     {
       name: "Prenotazioni",
-      color: "light"
+      color: "light",
+      element: "prenotazioni"
     },
   ]
 
-  constructor() { }
+  constructor(private domCtrl: DomController, private render: Renderer2) { }
 
   scrollTo(elementId: string) {
     let y = document.getElementById(elementId).offsetTop;
     this.content.scrollToPoint(0, y, 1000);
+  }
+
+  onScroll($event) {
+    let moveImage = 0;
+    let scrollTop = $event.detail.scrollTop;
+
+    this.domCtrl.write(() => {
+      if (scrollTop > 0) {
+        moveImage = scrollTop / 2;
+      }
+      console.log("scroll", scrollTop, moveImage, this.image);
+
+
+      this.render.setStyle(this.image.nativeElement, 'webkitTransform', 'translate3d(0' + moveImage + 'px, 0')
+    })
   }
 }
