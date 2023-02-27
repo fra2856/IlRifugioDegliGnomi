a14bbe557013b2ef58271f86867c70abe454f6ff
msavina <massimo.savina@gmail.com>
2023-02-27 22:11:10 +0100

    opacity navbar and css rename

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,4 @@
-import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
-import { DomController, IonContent } from '@ionic/angular';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -9,25 +7,20 @@ import { DomController, IonContent } from '@ionic/angular';
 })
 export class HomePage {
 
-  @ViewChild("parallax", { read: ElementRef }) image: ElementRef;
+  // @ViewChild("parallax", { read: ElementRef }) image: ElementRef;
 
   public opacityVal: number = 1;
 
-
-
-  constructor(private domCtrl: DomController, private render: Renderer2, private router: Router) { }
+  constructor() { }
 
   onScroll($event) {
     let scrollTop = $event.detail.scrollTop;
 
     // this.domCtrl.write(() => {
-    if (scrollTop > 0) {
+    if (scrollTop >= 0) {
       this.opacityVal = 1 - scrollTop / 200;
     }
-    console.log("scroll", scrollTop, this.opacityVal);
-    // console.log(this.moveImage);
-
-
+    // console.log("scroll", scrollTop, this.opacityVal);
     // this.render.setStyle(this.image.nativeElement, 'transform', `translateY(${this.moveImage}px)`)
     // })
   }
