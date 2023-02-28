579521a6ab73ca11290130e853a1b7b37a257d7e
msavina <massimo.savina@gmail.com>
2023-02-28 23:29:48 +0100

    hamburger menu list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { Platform } from '@ionic/angular';
+import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonContent, MenuController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,52 @@ import { Platform } from '@ionic/angular';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(public platform: Platform) { }
+  constructor(public platform: Platform, private router: Router, private menuController: MenuController) { }
+
+  @ViewChild(IonContent, { static: false }) content: IonContent
+
+  pages = [
+    {
+      name: "Home",
+      color: "light",
+      action: () => this.scrollTo("home"),
+    },
+    {
+      name: "Dove siamo",
+      color: "light",
+      action: () => this.scrollTo("where"),
+
+    },
+    {
+      name: "Servizi",
+      color: "light",
+      action: () => this.scrollTo("services"),
+
+    },
+    {
+      name: "Camere",
+      color: "light",
+      action: () => this.goTo("/rooms"),
+
+    },
+    {
+      name: "Prenotazioni",
+      color: "light",
+      action: () => this.goTo("/reservations"),
+
+    },
+  ]
+
+  goTo(route: string) {
+    console.log(route);
+    this.router.navigate([route]);
+  }
+
+  scrollTo(elementId: string) {
+    let y = document.getElementById(elementId).offsetTop;
+    this.menuController.close();
+    this.content.scrollToPoint(0, y, 1000);
+    console.log(y, this.content);
+
+  }
 }
